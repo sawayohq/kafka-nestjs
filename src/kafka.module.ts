78110a2d0476d31d47fcb40cd4099328af2ebd18
behrad kazemi <behradkzm@gmail.com>
2025-06-16 15:01:07 +0330

    fix(kafka): remove forProducer and make kafkaModule global

--- src/kafka.module.ts
+++ src/kafka.module.ts
@@ -12,22 +12,6 @@ import { SharedKafkaAsyncConfiguration } from "./interfaces/shared-queue-async-c
 @Module({})
 export class KafkaModule {
 
-  static forProducer(): DynamicModule {
-    return {
-      module: KafkaModule,
-      imports: [DiscoveryModule],
-      providers: [
-        {
-          provide: 'KAFKA_PARTITIONER',
-          useValue: undefined,
-        },
-        KafkaProducerService,
-        KafkaExplorer,
-      ],
-      exports: [KafkaProducerService, KafkaExplorer],
-    };
-  }
-
   static forRootAsync(options: SharedKafkaAsyncConfiguration): DynamicModule {
     const asyncProvider: Provider = {
       provide: KAFKA_MODULE_OPTIONS,
