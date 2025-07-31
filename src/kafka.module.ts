67e7c51019fa96ffaeb14516dc6ca6533f13e0ba
Vinay Bedre <vinay.bedre@sawayo.de>
2025-07-31 19:46:51 +0200

    feature: Add discovery module

--- src/kafka.module.ts
+++ src/kafka.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { DiscoveryModule } from "@nestjs/core";
 import { ConfigurableModuleClass } from "./kafka.module-definition";
 import { KafkaMetadataAccessor } from "./services/kafka-metadata.accessor";
 import { KafkaProducerService } from "./services/kafka-producer.service";
 import { KafkaExplorer } from "./services/kafka.explorer";
 
 @Module({
+  imports: [DiscoveryModule],
   exports: [KafkaProducerService, KafkaExplorer],
   providers: [
     {
